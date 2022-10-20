fb73244c832db3cc47b9af24f7eb2ecd4030fb18
rednarfsajor <franderrojas20@gmail.com>
2022-10-19 23:55:20 -0600

    Avance Interfaz Grafica Prueba 1
    70%

--- Prueba 1/Grupo4_Prueba1/src/main/java/grupo4/grupo4_prueba1/Cola.java
+++ Prueba 1/Grupo4_Prueba1/src/main/java/grupo4/grupo4_prueba1/Cola.java
@@ -2,8 +2,40 @@
 package grupo4.grupo4_prueba1;
 
 
-public class Cola {
+public class Cola<T> {
     private Nodo<T> Head;
     private Nodo<T> Tail;
     
+    public void AgregarOrden(Orden order){
+        Nodo newnodo = new Nodo<Orden>(order);
+        
+        if(Head==null){
+            Head=newnodo;
+            Tail=newnodo;
+            newnodo.setNext(null);
+        }
+        else{
+            if(order.isDiscapacidad()){
+                newnodo.setNext(Head);
+                Head=newnodo;
+            }
+            else{
+                Tail.setNext(newnodo);
+                Tail=newnodo;
+                newnodo.setNext(null);
+            }
+        }
+    }
+    
+    public void AtenderOrder(){
+        if(Head==null){
+            //Imprimo que ya no hay ordenes que atender
+        }
+        else{
+            Nodo Aux=Head;
+            Head=Head.getNext();
+            
+            Plato plato=new Plato();
+        }
+    }
 }
--- Prueba 1/Grupo4_Prueba1/src/main/java/grupo4/grupo4_prueba1/Plato.java
+++ Prueba 1/Grupo4_Prueba1/src/main/java/grupo4/grupo4_prueba1/Plato.java
@@ -3,7 +3,10 @@ package grupo4.grupo4_prueba1;
 
 
 public class Plato {
-    private int ID
-            
-            ;
+    private int ID;
+    private boolean Cubiertos;
+    
+    
+    
+    
 }
